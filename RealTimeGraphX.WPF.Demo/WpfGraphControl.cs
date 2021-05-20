6804e0080bd4f4fc4f2ae5541fd326864b0b6bc8
huypv <huy.pv@citigo.com.vn>
2021-05-20 17:31:12 +0700

    .net core support

--- RealTimeGraphX.WPF.Demo/WpfGraphControl.cs
+++ RealTimeGraphX.WPF.Demo/WpfGraphControl.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace RealTimeGraphX.WPF.Demo
 {
-    public class WpfGraphControl : Control
+    public class WpfGraphControl : System.Windows.Controls.Control
     {
         /// <summary>
         /// Gets or sets the graph controller.
--- RealTimeGraphX/GraphController.cs
+++ RealTimeGraphX/GraphController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Drawing;
 using RealTimeGraphX.EventArguments;
 using RealTimeGraphX.Renderers;
